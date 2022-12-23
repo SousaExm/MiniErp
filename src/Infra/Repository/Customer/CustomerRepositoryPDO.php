c1b0d694c81cd1f7f84ae96e0f046964dcfc00be
Renato Sousa <94645489+SousaExm@users.noreply.github.com>
2022-12-23 12:48:37 -0300

    up: implementando repositório de clients PDO

---
+++ src/Infra/Repository/Customer/CustomerRepositoryPDO.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace MiniErp\Infra\Repository\Customer;
+
+use DateTimeImmutable;
+use MiniErp\Domain\Customer\Customer;
+use MiniErp\Domain\Repository\Common\AddressRepository;
+use MiniErp\Domain\Repository\Common\PhonesRepository;
+use MiniErp\Domain\Repository\Customer\CustomerRepository;
+use PDO;
+
+class CustomerRepositoryPDO implements CustomerRepository
+{
+  private PDO $pdo;
+  private AddressRepository $addressRepository;
+
+  private PhonesRepository $phonesRepository;
+
+  public function __construct(PDO $pdo, AddressRepository $addressRepository, PhonesRepository $phonesRepository)
+  {
+    $this->pdo = $pdo;
+    $this->addressRepository = $addressRepository; 
+    $this->phonesRepository = $phonesRepository;
+  }
+  
+	/**
+	 * @return Customer[]
+	 */
+	public function allCustomers(): array 
+  {
+    $query = 'SELECT * FROM customer';
+    $stmt = $this->pdo->prepare($query);
+    $stmt->execute();
+    $customerDataList =  $stmt->fetchAll(PDO::FETCH_ASSOC);  
+    return $this->hydrateCustomersList($customerDataList);
+	}
+
+  private function hydrateCustomersList(array $customerDataList): array
+  {
+    $customersList = [];  
+    foreach($customerDataList as $customerData) {      
+      $customersList[] = $this->hydrateCustomer($customerData);
+    }
+    return $customersList;
+  }
+
+  private function hydrateCustomer($customerData): Customer | null
+  {   
+    $customer = new Customer(
+      $customerData['name'],
+      $customerData['cpf'],
+      $customerData['email'],
+      new DateTimeImmutable($customerData['birthDate']),
+      $customerData['id']
+    );
+      $customerWithPhone = $this->phonesRepository->getPhonesFor($customer);
+      $customerWithPhoneAndAdress = $this->addressRepository->getAddressFor($customerWithPhone);
+
+    return $customerWithPhoneAndAdress;
+  }
+
+  public function getCustomerById(string $id): Customer | false 
+  {
+    $query = 'SELECT * FROM customer
+    WHERE id = :customerId';
+    $stmt = $this->pdo->prepare($query);
+    $stmt->execute([
+    ':customerId' => $id
+    ]);
+
+    $customerData = $stmt->fetch(PDO::FETCH_ASSOC);    
+    if($customerData == false){
+    return false;
+    }
+
+  return $this->hydrateCustomer($customerData);
+	}
+	
+	public function getCustomerByCpf(string $cpf): Customer | false
+  {
+    $query = 'SELECT * FROM customer
+              WHERE cpf = :customerCpf';
+    
+    $stmt = $this->pdo->prepare($query);
+    $stmt->execute([
+      ':customerCpf' => $cpf
+    ]);
+
+    $customerData = $stmt->fetch(PDO::FETCH_ASSOC);    
+    if($customerData == false){
+      return false;
+    }
+
+    return $this->hydrateCustomer($customerData);
+	}
+
+  public function save(Customer $customer): Customer 
+  {
+    if(!$this->getCustomerByCpf($customer->cpf())){
+      return $this->insert($customer);
+    };
+
+    return $this->update($customer);
+	}
+	
+	public function remove(Customer $customer): void 
+  {
+    if($customer->uuid() === ''){
+      throw new \InvalidArgumentException('Para excluir um usuário é necessário informar o seu ID');
+    }
+
+    $this->phonesRepository->remove($customer);
+    $this->addressRepository->remove($customer);
+    
+    $query = 'DELETE from customer
+              WHERE id = :customerUuid';
+    $stmt = $this->pdo->prepare($query);
+    $stmt->execute([
+      ':customerUuid' => $customer->uuid()
+    ]);
+	}
+
+  private function insert(Customer $customer): Customer
+  {
+    $customer->generateUuid();
+    $query = 'INSERT INTO customer
+              values (:id, :cpf, :name, :email, :birthDate)';
+              
+    $stmt = $this->pdo->prepare($query);
+    $stmt->execute([
+      ':id' => $customer->uuid(),
+      ':name' => $customer->name(),
+      ':cpf' => $customer->cpf(),
+      ':email' => $customer->email(),
+      ':birthDate' => $customer->birthDate()
+    ]);
+
+    $this->phonesRepository->save($customer);
+    $this->addressRepository->save($customer);
+    return $customer;
+  }
+  
+  private function update(Customer $customer): Customer
+  {
+    if($customer->uuid() === ''){
+      throw new \InvalidArgumentException('Para atualizar um usuário é necessário informar o seu ID');
+    }
+    $query = 'UPDATE customer
+              SET name = :name, email = :email
+              WHERE id = :id';
+              
+    $stmt = $this->pdo->prepare($query);
+    $stmt->execute([
+      ':id' => $customer->uuid(),
+      ':name' => $customer->name(),
+      ':email' => $customer->email()
+    ]);
+
+    $this->phonesRepository->save($customer);
+    $this->addressRepository->save($customer);
+    return $this->getCustomerByCpf($customer->cpf());
+  }
+}
