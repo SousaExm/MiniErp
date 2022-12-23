13aa68cdd3fb8b3c782b3537be29d044ad01adcb
Renato Sousa <94645489+SousaExm@users.noreply.github.com>
2022-12-23 11:57:07 -0300

    add: implementando interface de repositório de clientes

---
+++ src/Domain/Repository/Customer/CustomerRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MiniErp\Domain\Repository;
+use MiniErp\Domain\Customer\{
+  Customer,
+  Cpf
+};
+
+interface CustomerRepository
+{
+  /**
+   * @return Customer[]
+   */
+  public function allCustomers(): array;
+  public function getCustomerByCpf(string $cpf): Customer | false;
+  public function getCustomerById(string $id): Customer | false;
+  public function remove(Customer $customer): void;
+  public function save(Customer $customer): Customer;
+}
